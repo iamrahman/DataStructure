76b47dd0111f46370225a4e4f8ea681ee0e9d064
iamrahman <inamur.rahman41@gmail.com>
2019-04-22 11:44:05 +0530

    Remove Duplicate

--- Array/find_duplicate_in_an_array.cpp
+++ Array/find_duplicate_in_an_array.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 int main(){
 	int temp = 0;
+	int k =0;
 	int A[10] = {6,2,1,8,5,9,2,4,5,3};
+	cout<<"Original Array: "<<"\n";
+	for(int i =0; i<10; i++){
+			cout<<A[i]<<" ";
+	}
 	
 	for(int i =0; i<10; i++){
 		for(int j =0; j<10; j++){
@@ -15,10 +20,24 @@ int main(){
 				}
 			}
 		}
-	cout<<"Duplicate Number: "<<"\n";
+	cout<<"\nDuplicate Number: "<<"\n";
 	for(int i =0; i<10; i++){
 		if(A[i] == A[i+1]){
 			cout<<A[i]<<" ";
 		}
 	}
+	cout<<"\n\nRemove Duplicate\n";
+	for(int i =0; i<10; i++){
+		if(A[i] != A[i+1]){
+			A[k] = A[i];
+			k++;
+		}
+	}
+	A[k] = A[10];
+	cout<<"Final Array: "<<"\n";
+	for(int i =0; i<k; i++){
+			cout<<A[i]<<" ";
+	}
+	
+	
 }
