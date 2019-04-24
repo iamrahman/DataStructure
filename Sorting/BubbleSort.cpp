c9bc4e93e93fa12c23cefbbe12b63e161af7cb2e
iamrahman <inamur.rahman41@gmail.com>
2019-04-24 13:16:51 +0530

    bubble sort few changes

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -14,14 +14,14 @@ void swapElement(int *xp, int *yp)
 void bubbleSorting(int arr[], int n) 
 { 
    int i, j; 
-   b swapped; 
+   bool swapped; 
    for (i = 0; i < n-1; i++) 
    { 
      swapped = false; 
      for (j = 0; j < n-i-1; j++) 
-     { 
+     {
         if (arr[j] > arr[j+1]) 
-        { 
+        {
            swapElement(&arr[j], &arr[j+1]); 
            swapped = true; 
         } 
@@ -29,7 +29,7 @@ void bubbleSorting(int arr[], int n)
      
      if (swapped == false) 
         break; 
-   } 
+   }
 }
 
 
